81a7f840694a81380dfe638047448cc923ec7683
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-18 15:40:39 -0500

    Connect model get methods to collection get helpers

--- server/database/index.js
+++ server/database/index.js
@@ -6,40 +6,40 @@ const { ProductSchema, CartItemSchema } = require('./schema');
 const CartItems = connection.model('cart_item', CartItemSchema);
 const Products = connection.model('product', ProductSchema);
 
-// // cart methods
-// const cart = {
-//   get: (item = {}) => {
-//     return CartItems.find(item).exec();
-//   },
-//   add: (item) => {
-//     let product;
-//     let query;
-//     return products.get({ id: item.id })
-//       .then(([ productDetails ]) => product = productDetails)
-//       .then(() => cart.get({ id: product.id }))
-//       .then((cartQuery) => query = cartQuery)
-//       .then(() => {
-//         const cartItem = JSON.parse(JSON.stringify(product));
-//         cartItem.quantity = item.quantity;
+// collection methods
+const methods = require('./methods');
 
-//         if (query.length === 0) {
-//           return CartItems.create(cartItem)
-//         }
+// cart interface
+const cart = {
+  get: (filter) => methods.get(CartItems, filter),
+  // add: (item) => {
+  //   let product;
+  //   let query;
+  //   return products.get({ id: item.id })
+  //     .then(([ productDetails ]) => product = productDetails)
+  //     .then(() => cart.get({ id: product.id }))
+  //     .then((cartQuery) => query = cartQuery)
+  //     .then(() => {
+  //       const cartItem = JSON.parse(JSON.stringify(product));
+  //       cartItem.quantity = item.quantity;
 
-//         const newQty = query[0].quantity + item.quantity;
-//         return CartItems.updateOne({ id: product.id }, { quantity: newQty });
-//       })
-//       .catch(console.error);
-//   },
-//   remove: (item) => {
-//     return CartItems.deleteOne(item).exec();
-//   },
-// };
+  //       if (query.length === 0) {
+  //         return CartItems.create(cartItem)
+  //       }
+
+  //       const newQty = query[0].quantity + item.quantity;
+  //       return CartItems.updateOne({ id: product.id }, { quantity: newQty });
+  //     })
+  //     .catch(console.error);
+  // },
+  // remove: (item) => {
+  //   return CartItems.deleteOne(item).exec();
+  // },
+};
 
 // // product methods
-// const products = {
-//   get: (item = {}) => {
-//     return Products.find(item).exec();
-//   },
-// };
+const products = {
+  get: (filter) => methods.get(Products, filter),
+};
 
+module.exports = { cart, products };
