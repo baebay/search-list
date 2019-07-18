4d24ab13bc9301073231ec11e73efd5418627203
Ross Calimlim <ross.calimlim@gmail.com>
2019-07-18 15:09:10 -0500

    Comment test file

--- test/database.test.js
+++ test/database.test.js
@@ -1,12 +1,17 @@
+// invoke should assertion wrapper
 const should = require('chai').should();
+
+// require mongoose server and fake mongodb
 const mongoose = require('mongoose');
 const connection = mongoose.connection;
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const methods = require('../server/database/methods');
+
+// require collection schemas, data, and helper methods
 const { ProductSchema, CartItemSchema } = require('../server/database/schema');
-
 const productData = require('./data/data');
+const methods = require('../server/database/methods');
 
+// declare models
 let Products;
 
 before((done) => {
@@ -28,23 +33,27 @@ after(() => {
 });
 
 describe('Collection helper methods', () => {
-  console.log(Products);
   describe('methods.get()', () => {
-
-    it('Should return a promise', () => {
+    it('should return a promise', () => {
       methods.get(Products, {}).should.be.a('promise');
     });
 
-    it('Should query the passed in collection', async () => {
+    it('should query the passed in collection', async () => {
       const expected = await Products.find({});
       const actual = await methods.get(Products, {});
       actual.should.deep.equal(expected);
     });
 
-    it('Should default to a blank filter if none is passed', async () => {
+    it('should default to a blank filter if none is passed', async () => {
       const expected = await Products.find();
       const actual = await methods.get(Products);
       actual.should.deep.equal(expected);
+    });
+
+    it('should query a collection based on a filter', async () => {
+      const expected = await Products.findOne();
+      const [ actual ] = await methods.get(Products, { id: expected.id });
+      actual.name.should.equal(expected.name);
     });
   });
 });
