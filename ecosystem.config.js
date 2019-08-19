9cef7a81d32885233609ecc3208e9fc268c1238d
Ross Calimlim <ross.calimlim@gmail.com>
2019-08-19 18:41:59 -0500

    Fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   apps: [{
     name: 'seabay-cart',
-    script: './server/server.js'
+    script: './server/server.js',
   }],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'seabay.rosscalimlim.me',
-      key: '~/.ssh/seabay.pem',
+      user: 'ross',
+      host: '134.209.69.8',
+      key: '~/.ssh/rosscalimlim.me',
       ref: 'origin/master',
-      repo: 'git@github.com:baebay/shopping-cart.git',
-      path: '/home/ubuntu/server/shopping-cart',
-      'post-deploy': 'npm install && npx webpack --config ./webpack.config.js && pm2 startOrRestart ecosystem.config.js'
-
-    }
-  }
-}
+      repo: 'git@github.com:seabay-hratx42-fec/shopping-cart.git',
+      path: '/home/ross/server/shopping-cart',
+      'post-deploy': 'npm install && npx webpack --config ./webpack.config.js && pm2 startOrRestart ecosystem.config.js',
+    },
+  },
+};
