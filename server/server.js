9bfc6121810210c82b9e2a8737fb0687561fb2de
Ross Calimlim <ross.calimlim@gmail.com>
2019-08-19 21:16:24 -0500

    Fix server routes

--- server/server.js
+++ server/server.js
@@ -14,42 +14,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/projects/seabay/shopping-cart/app', express.static('client/dist'));
 
 // http get /api/cart/items --> all cart items
-app.get('/api/cart/items', (req, res) => {
+app.get('/projects/seabay/shopping-cart/app/api/cart/items', (req, res) => {
   db.cart.get()
     .then((items) => res.status(200).send(items))
     .catch((err) => res.status(400).send());
 });
 
 // http get /api/cart/items/{id} --> single item from cart
-app.get('/api/cart/items/:id', (req, res) => {
+app.get('/projects/seabay/shopping-cart/app/api/cart/items/:id', (req, res) => {
   db.cart.get({ id: req.params.id })
     .then((item) => res.status(200).send(item))
     .catch((err) => res.status(400).send());
 });
 
 // http post /api/cart/items/ --> single item to cart
-app.post('/api/cart/items/', (req, res) => {
+app.post('/projects/seabay/shopping-cart/app/api/cart/items/', (req, res) => {
   db.cart.add({ id: req.body.id, quantity: req.body.quantity })
     .then(() => res.status(201).send())
     .catch((err) => res.status(400).send());
 });
 
 // http delete /api/cart/items/{id} --> delete single item from cart
-app.delete('/api/cart/items/:id', (req, res) => {
+app.delete('/projects/seabay/shopping-cart/app/api/cart/items/:id', (req, res) => {
   db.cart.remove({ id: req.params.id })
     .then(() => res.status(200).send())
     .catch((err) => res.status(400).send());
 });
 
 // http get /api/products/ --> all products
-app.get('/api/products', (req, res) => {
+app.get('/projects/seabay/shopping-cart/app/api/products', (req, res) => {
   db.products.get()
     .then((products) => res.status(200).send(products))
     .catch((err) => res.status(400).send());
 });
 
 // http get /api/products/{id} --> dingle product from products
-app.get('/api/products/:id', (req, res) => {
+app.get('/projects/seabay/shopping-cart/app/api/products/:id', (req, res) => {
   db.products.get({ id: req.params.id })
     .then((product) => res.status(200).send(product))
     .catch((err) => res.status(400).send());
